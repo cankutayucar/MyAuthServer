be59b7cbb97059db5c3c7def9d4bc7710dbf2866
cankutay <cankutayucar@hotmail.com>
2022-07-11 13:07:20 +0300

    generic service added

--- CankutayUcarAuthServer.Core/Services/IGenericService.cs
+++ CankutayUcarAuthServer.Core/Services/IGenericService.cs
@@ -14,8 +14,8 @@ namespace CankutayUcarAuthServer.Core.Services
         Task<Response<TDto>> GetByIdAsync(int id);
         Task<Response<IEnumerable<TDto>>> GetAllAsync();
         Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> expression);
-        Task<Response<TDto>> AddAsync(TEntity entity);
-        Task<Response<NoDataDto>> Remove(TEntity entity);
-        Task<Response<NoDataDto>> Update(TEntity entity);
+        Task<Response<TDto>> AddAsync(TDto dto);
+        Task<Response<NoDataDto>> Remove(int id);
+        Task<Response<NoDataDto>> Update(TDto dto, int id);
     }
 }
---
+++ CankutayUcarAuthServer.Service/Services/GenericService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CankutayUcarAuthServer.Core.Repositories;
+using CankutayUcarAuthServer.Core.Services;
+using CankutayUcarAuthServer.Core.UnitOfWork;
+using CankutayUcarAuthServer.Service.Mappers;
+using CankutayUcarAuthServer.Shared.Dtos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+
+namespace CankutayUcarAuthServer.Service.Services
+{
+    public class GenericService<TEntity, TDto> : IGenericService<TEntity, TDto>
+        where TEntity : class where TDto : class
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<TEntity> _repository;
+
+        public GenericService(IUnitOfWork unitOfWork, IGenericRepository<TEntity> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+
+        public async Task<Response<TDto>> GetByIdAsync(int id)
+        {
+            var product = await _repository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return Response<TDto>.Fail("id not found", 404, true);
+            }
+
+            return Response<TDto>.Success(ObjectMapper.Mapper.Map<TDto>(product), 200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> GetAllAsync()
+        {
+            var products = ObjectMapper.Mapper.Map<List<TDto>>(await _repository.GetAllAsync());
+            return Response<IEnumerable<TDto>>.Success(products, 200);
+        }
+
+        public async Task<Response<IEnumerable<TDto>>> Where(Expression<Func<TEntity, bool>> expression)
+        {
+            var list = _repository.Where(expression);
+            return Response<IEnumerable<TDto>>.Success(ObjectMapper.Mapper.Map<IEnumerable<TDto>>(await list.ToListAsync()), 200);
+        }
+
+        public async Task<Response<TDto>> AddAsync(TDto dto)
+        {
+            var newEntity = ObjectMapper.Mapper.Map<TEntity>(dto);
+            await _repository.AddAsync(newEntity);
+            await _unitOfWork.CommitAsync();
+            var newDto = ObjectMapper.Mapper.Map<TDto>(newEntity);
+            return Response<TDto>.Success(newDto, 200);
+        }
+
+        public async Task<Response<NoDataDto>> Remove(int id)
+        {
+            var isExistEntity = await _repository.GetByIdAsync(id);
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("id not found", 404, true);
+            }
+            _repository.Remove(isExistEntity);
+            await _unitOfWork.CommitAsync();
+            return Response<NoDataDto>.Success(204);
+        }
+
+        public async Task<Response<NoDataDto>> Update(TDto dto, int id)
+        {
+            var isExistEntity = await _repository.GetByIdAsync(id);
+            if (isExistEntity == null)
+            {
+                return Response<NoDataDto>.Fail("id not found", 404, true);
+            }
+            var newEntity = ObjectMapper.Mapper.Map<TEntity>(dto);
+            _repository.Update(newEntity);
+            await _unitOfWork.CommitAsync();
+            return Response<NoDataDto>.Success(204);
+        }
+    }
+}
