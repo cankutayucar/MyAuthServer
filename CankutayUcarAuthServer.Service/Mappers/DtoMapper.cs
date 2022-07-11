eac9ff600843f2f03ad33538f2e355e55d084489
cankutay <cankutayucar@hotmail.com>
2022-07-11 11:01:50 +0300

    addad automapper

---
+++ CankutayUcarAuthServer.Service/Mappers/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CankutayUcarAuthServer.Core.Dtos;
+using CankutayUcarAuthServer.Core.Models;
+
+namespace CankutayUcarAuthServer.Service.Mappers
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
